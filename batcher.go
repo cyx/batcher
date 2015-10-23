e3b14aa81c62a016d44e810b8fdba86739d333fa
Cyril David <cyril@heroku.com>
2015-10-23 15:41:16 -0700

    MVP

--- batcher.go
+++ batcher.go
@@ -1,27 +1,94 @@
 package batcher
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
+// QueueSize is the size of the queue channel and should depend based
+// on the load volume that you expect.
+//
+// If N=1500 and QueueSize=100, that means the max bandwidth you can
+// handle is 150K pushes / second.
+var QueueSize = 100
+
+// Batcher gives you a generic concept of: Push, Trigger, Close.
 type Batcher interface {
-	Push(elem interface{}) error
-	Next() <-chan Batch
+	Queue(elem interface{}) error
+	Trigger(func(chan interface{}))
+	Close()
 }
 
+// New gives you a initialized Batcher with batcher.QueueSize
+// being the only pre-configured default. You can change that default
+// by doing:
+//
+//         batcher.QueueSize = N
 func New(count int, interval time.Duration) Batcher {
-	return &batcher{count, interval}
+	return &batcher{
+		count:    count,
+		interval: interval,
+		list:     make(chan interface{}, count),
+		queue:    make(chan chan interface{}, QueueSize),
+		closer:   make(chan struct{}),
+	}
 }
 
 type batcher struct {
+	sync.Mutex
+
 	count    int
 	interval time.Duration
+	list     chan interface{}
+	queue    chan chan interface{}
+	closer   chan struct{}
 }
 
-func (b *batcher) Push(elem interface{}) error {
+func (b *batcher) Queue(elem interface{}) error {
+	if len(b.list) >= b.count {
+		b.queue <- b.unbatch()
+	}
+	b.list <- elem
+
+	// No known errors to return at this time, but we
+	// should reserve the right to have one for posterity.
 	return nil
 }
 
-func (b *batcher) Next() <-chan Batch {
-	return nil
+func (b *batcher) unbatch() chan interface{} {
+	b.Mutex.Lock()
+	defer b.Mutex.Unlock()
+
+	// Copy the channel
+	payload := b.list
+
+	// Re-initialize the list with an empty channel.
+	b.list = make(chan interface{}, b.count)
+
+	return payload
 }
 
-type Batch []interface{}
+func (b *batcher) Trigger(fn func(chan interface{})) {
+	for {
+		select {
+		case payload := <-b.queue:
+			// Happy path.
+			fn(payload)
+
+		case <-time.After(b.interval):
+			// Flush if we have anything, otherwise, we
+			// start all over again with the timer reset.
+			if len(b.list) >= 0 {
+				fn(b.unbatch())
+			}
+
+		case <-b.closer:
+			// Flush any other bits we might still have.
+			fn(b.list)
+		}
+	}
+}
+
+func (b *batcher) Close() {
+	close(b.closer)
+}
--- batcher_test.go
+++ batcher_test.go
@@ -1,5 +1,95 @@
 package batcher
 
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
 func init() {
 	var _ Batcher = &batcher{}
+}
+
+type myConcreteType struct {
+	Name string
+}
+
+func TestQueueTriggerBasics(t *testing.T) {
+	// Obscenely long time. We want the batch by number
+	// in this test.
+	b := New(5, time.Second*1500)
+
+	// Use channels to signal completion
+	done := make(chan chan interface{})
+	go b.Trigger(func(payload chan interface{}) {
+		done <- payload
+	})
+
+	// Make 5x+1 pushes to trigger a batch.
+	// The extra push is because the payload only triggers when trying
+	// to push to a full queue.
+	for i := 0; i < 6; i++ {
+		// Queue a hypothetical concrete type
+		b.Queue(myConcreteType{Name: fmt.Sprintf("John %d", i)})
+	}
+
+	select {
+	case payload := <-done:
+		if len(payload) != 5 {
+			t.Fatalf("Payload len expected to be 5, got %d", len(payload))
+		}
+
+	case <-time.After(time.Second * 1):
+		t.Fatal("Too long!")
+	}
+}
+
+func TestQueueTriggerTimedFlush(t *testing.T) {
+	// We want enough time to get a batch,
+	// but fast enough that the test won't block long.
+	b := New(5, time.Millisecond*10)
+
+	// Use channels to signal completion
+	done := make(chan chan interface{})
+	go b.Trigger(func(payload chan interface{}) {
+		done <- payload
+	})
+
+	// Queue a single hypothetical concrete type
+	b.Queue(myConcreteType{Name: "John"})
+
+	select {
+	case payload := <-done:
+		if len(payload) != 1 {
+			t.Fatalf("Payload len expected to be 1, got %d", len(payload))
+		}
+
+	case <-time.After(time.Second * 1):
+		t.Fatal("Too long!")
+	}
+}
+
+func TestQueueTriggerCloseFlush(t *testing.T) {
+	// We want the flush to be triggered by `Close()`.
+	b := New(5, time.Second*1500)
+
+	// Use channels to signal completion
+	done := make(chan chan interface{})
+	go b.Trigger(func(payload chan interface{}) {
+		done <- payload
+	})
+
+	// Queue a single hypothetical concrete type
+	b.Queue(myConcreteType{Name: "John"})
+	b.Close()
+
+	select {
+	case payload := <-done:
+		if len(payload) != 1 {
+			t.Fatalf("Payload len expected to be 1, got %d", len(payload))
+		}
+
+	case <-time.After(time.Second * 1):
+		t.Fatal("Too long!")
+	}
 }
